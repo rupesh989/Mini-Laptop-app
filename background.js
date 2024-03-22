6d4fa29577c5a684affcee4a87ef2f8d796e7a75
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-22 22:11:05 +0530

    Update background.js

--- background.js
+++ background.js
@@ -54,4 +54,40 @@ const Background = () => {
               opacity: 0.5,
               width: 2,
             },
-           
+            collisions: {
+              enable: false,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 3,
+              straight: true,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 1000,
+              },
+              value: 150,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default Background;
