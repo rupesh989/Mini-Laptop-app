0cc429764b46759c7b47942e69a3635d9e6e9b4a
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-22 22:10:57 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,57 @@
 
+import React from "react";
+import Particles from "react-tsparticles";
+
+// Live Background of project
+const Background = () => {
+  return (
+    <div className="absolute -z-10">
+      <Particles
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "#02113d",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 50,
+              enable: true,
+              opacity: 0.5,
+              width: 2,
+            },
+           
