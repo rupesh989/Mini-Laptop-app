651064ddf19901292d0457b54d982f88c93c724b
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-22 22:14:55 +0530

    Update keyboard.js

--- keyboard.js
+++ keyboard.js
@@ -55,4 +55,93 @@ const Keyboard = ({ handlePower }) => {
     "|",
     "pgup",
   ];
- 
+  const row4 = [
+    "caps",
+    "A",
+    "S",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    ":  ;",
+    "'' '",
+    "enter",
+    "pgdn",
+  ];
+  const row5 = [
+    "shift",
+    "Z",
+    "X",
+    "C",
+    "V",
+    "B",
+    "N",
+    "M",
+    "< ,",
+    "> .",
+    "? /",
+    "shift",
+    "end",
+  ];
+  const row6 = [
+    "ctrl",
+    "fn",
+    "icon",
+    "alt",
+    "space",
+    "alt",
+    "ctrl",
+    "<",
+    "updown",
+    ">",
+  ];
+  return (
+    <div className="keyboard flex flex-col basis-3/6 w-[90%] gap-1 shadow-inner p-1">
+      <div key={1} className="row1 row">
+        {row1.map((item) =>
+          item === "icon" ? (
+            <i
+              key={item}
+              onClick={handlePower}
+              className="fa fa-power-off key w-[5.4%]"
+              aria-hidden="true"
+            ></i>
+          ) : (
+            <div key={item} className="key w-[5.4%]">
+              {item}
+            </div>
+          )
+        )}
+      </div>
+      <div key={2} className="row2 row">
+        {row2.map((item) => (
+          <div key={item} className="key w-[6.2%]">
+            <div>{item[0]}</div>
+            <div>{item[1]}</div>
+          </div>
+        ))}
+      </div>
+      <div key={3} className="row3 row">
+        {row3.map((item) => (
+          <div key={item} className="key w-[6.2%]">
+            {item}
+          </div>
+        ))}
+      </div>
+      <div key={4} className="row4 row">
+        {row4.map((item) =>
+          item === "enter" ? (
+            <div key={item} className="key w-[11%]">
+              {item}
+            </div>
+          ) : (
+            <div key={item} className="key w-[6.2%]">
+              {item}
+            </div>
+          )
+        )}
+      </div>
+   
