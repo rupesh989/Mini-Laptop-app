40ba3eea080a676cabf0bb5179265fdb719be67b
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-23 23:01:47 +0530

    Update Screen1.js

--- Screen1.js
+++ Screen1.js
@@ -1,1 +1,18 @@
 
+import React from "react";
+import hp_Logo from "../assets/hp_Logo.png";
+
+// First screen - loading screen hplogo and spinner
+const Screen1 = () => (
+  <div className="screen1 h-full w-full flex flex-col justify-around items-center">
+    <figure className="w-1/5 h-1/5">
+      <img src={hp_Logo} alt="hp-logo" />
+    </figure>
+    <svg
+      role="status"
+      className="w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-black"
+      viewBox="0 0 100 101"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      
