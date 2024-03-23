da0aebfa8e012970a2487c59046ff50f133ed0bc
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-23 23:00:34 +0530

    Update BasePanel.js

--- BasePanel.js
+++ BasePanel.js
@@ -23,3 +23,30 @@ const BasePanel = ({ handlePower, handleFingerScan }) => {
     );
   };
  
+ const TouchPad = () => {
+    return (
+      <div className="touchpad flex justify-between basis-2/6 w-[90%] py-1">
+        <p className="probook uppercase font-semibold text-xxs sm:text-xs tracking-widest">
+          Probook
+        </p>
+        <div className="mouse basis-2/5 shadow-inner rounded-md"></div>
+        <div className="fingerLock basis-1/12">
+          <div
+            onClick={handleFingerScan}
+            className="h-[30%] w-3/4 ml-auto shadow-inner rounded-sm hover:bg-green-400 active:bg-inherit"
+          ></div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="base flex flex-col items-center basis-1/2 bg-gray-300 rounded-lg">
+      <Speaker />
+      <Keyboard handlePower={handlePower} />
+      <TouchPad />
+    </div>
+  );
+};
+
+export default BasePanel;
