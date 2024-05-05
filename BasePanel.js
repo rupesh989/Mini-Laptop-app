3f5d0cd6f223e5fe3dd8488f05279128858b5afd
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-05-05 23:13:08 +0530

    Update BasePanel.js

--- BasePanel.js
+++ BasePanel.js
@@ -2,25 +2,3 @@
 import React from "react";
 import Keyboard from "./Keyboard";
 
-// Speaker, keyboard and touchpad of Laptop (base panel)
-const BasePanel = ({ handlePower, handleFingerScan }) => {
-  const Speaker = () => {
-    return (
-      <div className="basis-1/6 flex flex-col w-[90%] mb-2">
-        <div className="joint basis-1/4 w-4/5 mx-auto relative border-b-4 border-x-2 border-black"></div>
-        <div className="speaker basis-3/4 overflow-hidden">
-          {[...Array(6)].map(() => (
-            <ul key={Math.random()} className="flex gap-1">
-              {[...Array(150)].map(() => (
-                <li key={Math.random()} className="w-1 h-1">
-                  .
-                </li>
-              ))}
-            </ul>
-          ))}
-        </div>
-      </div>
-    );
-  };
- 
- const TouchPad = () => {
