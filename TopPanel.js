adafa40e560471745371857102e6fca1f517416c
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-05-12 23:18:59 +0530

    Update TopPanel.js

--- TopPanel.js
+++ TopPanel.js
@@ -16,23 +16,7 @@ const TopPanel = ({
 }) => {
   return (
     <>
-      <div className="top flex relative justify-center items-center basis-1/2 bg-black rounded-lg border-2 border-gray-300 shadow-inner">
-        <Screens
-          power={power}
-          fingerScan={fingerScan}
-          handleCelebrate={handleCelebrate}
-          celebrate={celebrate}
-          handleShutDown={handleShutDown}
-          handleRestart={handleRestart}
-          shuttingDown={shuttingDown}
-        />
-        <figure className="absolute sm:w-6 xs:w-4 bottom-0">
-          <img src={hp_Logo} alt="hp_logo" />
-        </figure>
-        <figure className="absolute bg-white rounded-full sm:w-6 xs:w-4 top-0">
-          <img src={webcam} alt="webcam" />
-        </figure>
-      </div>
+     
     </>
   );
 };
