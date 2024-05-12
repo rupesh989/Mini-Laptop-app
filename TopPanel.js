33848bd5711ea621993d31b1c719d535ff64f504
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-05-12 23:19:16 +0530

    Update TopPanel.js

--- TopPanel.js
+++ TopPanel.js
@@ -1,18 +1,1 @@
 
-import React from "react";
-import hp_Logo from "../assets/hp_Logo.png";
-import webcam from "../assets/webcam.png";
-import Screens from "./Screens";
-
-// Camera, screen and hp logo of Laptop(top panel)
-const TopPanel = ({
-
-}) => {
-  return (
-    <>
-     
-    </>
-  );
-};
-
-export default TopPanel;
