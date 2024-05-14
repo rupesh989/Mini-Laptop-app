e253b7ebdec622593477dd51698116622faaa708
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-05-14 16:21:44 +0530

    Update Screen3.js

--- Screen3.js
+++ Screen3.js
@@ -58,36 +58,3 @@ const Screen3 = ({ fingerScan, handleFourth }) => {
     );
   };
 
-const Right = () => {
-    return (
-      <div className="right basis-2/12 flex flex-col justify-end">
-        <div className="basis-[10%] flex items-end cursor-pointer ml-auto text-slate-300 xs:gap-1 md:gap-2">
-          <i title="Internet" className="fa fa-wifi hover:text-white "></i>
-          <i
-            title="Battery"
-            className="fa fa-battery-full hover:text-white"
-          ></i>
-          <i
-            title="Power"
-            className="fa fa-power-off hover:text-white"
-            aria-hidden="true"
-          ></i>
-        </div>
-      </div>
-    );
-  };
-
-  return (
-    <div className="screen3 bg-cover bg-coverBlur bg-no-repeat bg-center h-full w-full flex p-[1%]">
-      <Left />
-      <div className="middle basis-8/12 text-white flex flex-col justify-center xs:gap-1 sm:gap-2 items-center">
-        <img
-          className={`${
-            focus ? "xs:invisible" : "xs:visible"
-          } sm:visible rounded-full w-[20%] h-1/4`}
-          src={profile}
-          alt="profile"
-        />
-        <h1>Rupesh Bavaskar</h1>
-
- 
