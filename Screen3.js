b6b0e67110edbbc767c6a59920e632762ef77b23
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-05-15 22:41:07 +0530

    Update Screen3.js

--- Screen3.js
+++ Screen3.js
@@ -1,15 +1,1 @@
-import React, { useEffect, useState } from "react";
-import profile from "../assets/Profile.png";
-import user from "../assets/user.png";
-import { BiFingerprint } from "react-icons/bi";
-
-// Third screen - sign in screen of user
-const Screen3 = ({ fingerScan, handleFourth }) => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [welcome, setWelcome] = useState(false);
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
-  const [focus, setFocus] = useState(false);
-
-
 
