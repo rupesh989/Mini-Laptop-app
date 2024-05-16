07e15e0eddf4b7af59d8cce531255e990b504001
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-05-16 22:25:12 +0530

    Update Screen2.js

--- Screen2.js
+++ Screen2.js
@@ -1,14 +1,1 @@
-import React from "react";
-const addZero = (value) => {
-  if (value < 10) {
-    value = "0" + value;
-  }
-  return value;
-};
 
-// Second screen - cover screen with date and time
-const Screen2 = ({ handleClick }) => (
-  
-);
-
-export default Screen2;
